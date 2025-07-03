12248538173bdbfe2ce8e71ee3320e5489a1a84e
Ramadhan Nezz <108265300+ramadhannezz@users.noreply.github.com>
2025-07-03 12:05:02 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,9 +17,10 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="payment-list" element={<PaymentList />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
-          <Route path="payment-list" element={<PaymentList />} />
+          
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
